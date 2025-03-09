347e114cad752c93ba84f7a35c3b88bac417ddae
Julian Prokay <jprokay@gmail.com>
2025-03-09 16:23:38 -0700

    style: Simplify Player component styling and button colors

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -282,8 +282,8 @@ const Player: Component<Props> = (props) => {
           </div>
         </Show>
 
-        <fieldset class="fieldset border border-gray-300 rounded-md p-4 w-full">
-          <legend class="fieldset-legend px-2 text-sm font-medium text-gray-700">Video URL</legend>
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">Video URL</legend>
           <div class="join w-full flex flex-wrap md:flex-nowrap">
             <div class="join-item w-full md:w-4/5">
               <input
@@ -302,18 +302,18 @@ const Player: Component<Props> = (props) => {
 
               <div class="validator-hint hidden">Enter valid email address</div>
             </div>
-            <button type="button" class="btn btn-secondary join-item w-full md:w-1/5" onClick={() => setVideo("videoUrl", "")}>Clear</button>
+            <button type="button" class="btn btn-accent join-item w-full md:w-1/5" onClick={() => setVideo("videoUrl", "")}>Clear</button>
             <input class="input" name="videoId" value={video.videoId} readonly={true} type="hidden" />
           </div>
 
 
         </fieldset>
-        <fieldset class="fieldset border border-gray-300 rounded-md p-4 w-full">
-          <legend class="fieldset-legend px-2 text-sm font-medium text-gray-700">Loop Name</legend>
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">Loop Name</legend>
           <input class="input w-full" type="text" name="loopName" value={props.loopName || "Chorus"} />
         </fieldset>
-        <fieldset class="fieldset border border-gray-300 rounded-md p-4 w-full">
-          <legend class="fieldset-legend px-2 text-sm font-medium text-gray-700">Start Minute : Start Second</legend>
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">Start Minute : Start Second</legend>
           <div class="join w-full flex flex-wrap md:flex-nowrap">
             <div class="join-item w-2/5 md:w-[40%]">
               <input
@@ -345,7 +345,7 @@ const Player: Component<Props> = (props) => {
               <div class="validator-hint hidden">Value must be greater than or equal to 0</div>
             </div>
 
-            <button type="button" class="btn btn-secondary join-item w-1/5 md:w-1/5" onClick={setStartToNow}>Now</button>
+            <button type="button" class="btn btn-accent join-item w-1/5 md:w-1/5" onClick={setStartToNow}>Now</button>
           </div>
 
         </fieldset>
@@ -402,7 +402,7 @@ const Player: Component<Props> = (props) => {
 
           <div class="join-item w-full sm:w-1/2">
             <button
-              class={`btn btn-accent w-full ${video.loop ? 'btn-active' : ''}`}
+              class={`btn btn-secondary w-full ${video.loop ? 'btn-active' : ''}`}
               type="button"
               onClick={() => setVideo("loop", (loop) => !loop)}
             >
@@ -413,8 +413,8 @@ const Player: Component<Props> = (props) => {
             </button>
           </div>
         </div>
-        <fieldset class="fieldset border border-gray-300 rounded-md p-4 w-full">
-          <legend class="fieldset-legend px-2 text-sm font-medium text-gray-700">End Minute : End Second</legend>
+        <fieldset class="fieldset">
+          <legend class="fieldset-legend">End Minute : End Second</legend>
           <div class="join w-full flex flex-wrap md:flex-nowrap">
             <div class="join-item w-2/5 md:w-[40%]">
               <input
@@ -446,7 +446,7 @@ const Player: Component<Props> = (props) => {
               <div class="validator-hint hidden">Value must be greater than or equal to 0</div>
             </div>
 
-            <button type="button" class="btn btn-secondary join-item w-1/5 md:w-1/5" onClick={setEndToNow}>Now</button>
+            <button type="button" class="btn btn-accent join-item w-1/5 md:w-1/5" onClick={setEndToNow}>Now</button>
           </div>
         </fieldset>
 
